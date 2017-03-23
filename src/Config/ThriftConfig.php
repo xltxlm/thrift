ddd1c062e8aeef19209d1779125e92130c0a9ba1
夏琳泰 <xltxlm@qq.com>
2017-03-23 11:55:18 +0800

    * 2017-03-23 thrift 的客户端生成再进一步简化

---
+++ src/Config/ThriftConfig.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xialintai
+ * Date: 2017/3/23
+ * Time: 11:45
+ */
+
+namespace xltxlm\thrift\Config;
+
+/**
+ * 服务器的链接配置
+ * Class ThriftConfig
+ * @package xltxlm\thrift\Config
+ */
+abstract class ThriftConfig
+{
+    //服务器类型定义
+    const HTTP = 'http';
+    const SOCKET = 'socket';
+
+    /** @var string 服务端的ip地址 */
+    protected $host = "127.0.0.1";
+    /** @var int 服务端提供的端口 */
+    protected $port = 80;
+    /** @var string 类型 */
+    protected $type = self::HTTP;
+
+    /**
+     * @return string
+     */
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $host
+     * @return static
+     */
+    public function setHost(string $host)
+    {
+        $this->host = $host;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPort(): int
+    {
+        return $this->port;
+    }
+
+    /**
+     * @param int $port
+     * @return static
+     */
+    public function setPort(int $port)
+    {
+        $this->port = $port;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     * @return static
+     */
+    public function setType(string $type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+}
--- src/Template/ThriftClient.php
+++ src/Template/ThriftClient.php
@@ -8,30 +8,12 @@ use Thrift\Transport\TSocket;
 use Thrift\Transport\THttpClient;
 use Thrift\Transport\TBufferedTransport;
 use Thrift\Exception\TException;
+use \xltxlm\thrift\Config\ThriftConfig;
 use <?=$this->getReflectionClass()->getName()?>;
 
 class <?=$this->getReflectionClass()->getShortName().ucfirst($this->getMethodName()->getName())?>
 
 {
-    /*
-     * Licensed to the Apache Software Foundation (ASF) under one
-     * or more contributor license agreements. See the NOTICE file
-     * distributed with this work for additional information
-     * regarding copyright ownership. The ASF licenses this file
-     * to you under the Apache License, Version 2.0 (the
-     * "License"); you may not use this file except in compliance
-     * with the License. You may obtain a copy of the License at
-     *
-     *   http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing,
-     * software distributed under the License is distributed on an
-     * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-     * KIND, either express or implied. See the License for the
-     * specific language governing permissions and limitations
-     * under the License.
-     */
-
     /** @var TBinaryProtocol */
     protected $transport;
     /** @var <?=$this->getReflectionClass()->getShortName()?> */
@@ -64,13 +46,12 @@ class <?=$this->getReflectionClass()->getShortName().ucfirst($this->getMethodNam
     /**
      * constructor.
      */
-    public function __construct( string $uri )
+    public function __construct( ThriftConfig $ThriftConfig )
     {
-        $parse_url = parse_url($uri);
-        if (strpos($uri, 'http') !== false) {
-            $socket = new THttpClient($parse_url['host'], $parse_url['port'] ?: 80, $parse_url['path'].'?'.$parse_url['query']);
+        if ($ThriftConfig->getType() == ThriftConfig::HTTP) {
+            $socket = new THttpClient($ThriftConfig->getHost(), $ThriftConfig->getPort(), '?c=<?=strtr($this->getReflectionClass()->getShortName(),['Client'=>''])?>/<?=$this->getReflectionClass()->getShortName().ucfirst($this->getMethodName()->getName())?>');
         } else {
-            $socket = new TSocket($uri);
+            $socket = new TSocket($ThriftConfig->getHost(), $ThriftConfig->getPort());
         }
         $this->transport = new TBufferedTransport($socket, 1024, 1024);
         $protocol = new TBinaryProtocol($this->transport);
